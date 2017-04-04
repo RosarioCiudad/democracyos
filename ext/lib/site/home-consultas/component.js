f78215fc407ef74014b2ef0ef384e79ff4f9443f
Matías Lescano <mjlescano@protonmail.com>
2017-04-04 14:05:36 -0300

    add home consultas

---
+++ ext/lib/site/home-consultas/component.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import forumStore from 'lib/stores/forum-store/forum-store'
+import topicStore from 'lib/stores/topic-store/topic-store'
+import userConnector from 'lib/site/connectors/user'
+import TopicCard from './topic-card/component'
+
+class HomePresupuesto extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      loading: true,
+      forum: null,
+      topics: null
+    }
+  }
+
+  fetchForums = () => {
+    forumStore.findOneByName('consultas')
+      .then((forum) => Promise.all([
+        forum,
+        topicStore.findAll({ forum: forum.id })
+      ])
+      .then(([forum, topics]) => {
+        this.setState({
+          loading: false,
+          forum,
+          topics
+        })
+      })
+      .catch((err) => {
+        this.setState({ loading: false })
+        throw err
+      })
+  }
+
+  render () {
+    const { forum, topics } = this.state
+    return (
+      <div className='ext-home-presupuesto'>
+        <div className='cover'>
+          <div className='container'>
+            <div className='isologo consultas'></div>
+            <h1>Consultas</h1>
+            <p>La Municipalidad quiere conocer tu opinión sobre<br />diferentes temáticas de nuestra Ciudad.</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default userConnector(HomePresupuesto)
