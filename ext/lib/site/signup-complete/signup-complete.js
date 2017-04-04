6cb9b7ad949fd46df32f4079705753a40c1209ba
Matías Lescano <mjlescano@protonmail.com>
2017-04-04 10:36:28 -0300

    update routes extension

--- ext/lib/site/signup-complete/signup-complete.js
+++ ext/lib/site/signup-complete/signup-complete.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import {Route} from 'react-router'
+import { Route } from 'react-router'
 import 'lib/boot/routes'
 import router from 'lib/site/boot/router'
 import SignupComplete from './component'
 
-const site = router.find((route) => route.key === 'lib-site')
-
-site.props.children.unshift(
+router.childRoutes.unshift(
   <Route
     key='ext-signup-complete'
     path='/signup/complete'
--- ext/lib/site/static-pages/static-pages.js
+++ ext/lib/site/static-pages/static-pages.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import {Route} from 'react-router'
+import { Route } from 'react-router'
 import 'lib/boot/routes'
 import router from 'lib/site/boot/router'
 import TerminosYCondiciones from './pages/terminos-y-condiciones'
 
-const site = router.find((route) => route.key === 'lib-site')
-
-site.props.children.unshift(
+router.childRoutes.unshift(
   <Route
     key='ext-terminos-y-condiciones'
     path='/s/terminos-y-condiciones'
