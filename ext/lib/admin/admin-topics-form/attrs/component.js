a6eabad2a8abd41aba97b7ef06cbbc46a0ec3c34
Luciano <lmilia0@rosario.gov.ar>
2018-09-18 11:03:41 -0300

    Agrego value Ver Noticias por defecto en linkText

--- ext/lib/admin/admin-topics-form/attrs/component.js
+++ ext/lib/admin/admin-topics-form/attrs/component.js
@@ -60,11 +60,12 @@ forms.String = ({
       className='form-control'
       type='text'
       name={`attrs.${name}`}
-      defaultValue={name === 'anio' ? (value || '2018') : value}
+      defaultValue={name === 'anio' ? (value || '2018') : (name=== 'linkText' ? (value || 'Ver Noticias') : value)}
       minLength={min}
       maxLength={max}
       required={mandatory}
-      validate={mandatory && 'required'} />
+      validate={mandatory && 'required'} 
+      />
   </div>
 )
 
