f0570f82f3e226757657df536a6e1718eb07d2a0
Luciano <lmilia0@rosario.gov.ar>
2018-11-22 09:46:16 -0300

    agregar override del limit

--- ext/lib/boot/overrides.js
+++ ext/lib/boot/overrides.js
@@ -3,3 +3,8 @@ import * as Header from 'lib/header/component'
 import * as HeaderOverride from 'ext/lib/header/component'
 
 Object.assign(Header, HeaderOverride)
+
+import * as topicMiddlewares from 'lib/middlewares/topic-middlewares/topic-middlewares'
+import * as topicMiddlewaresOverride from 'ext/lib/middlewares/topic-middlewares/topic-middlewares'
+
+Object.assign(topicMiddlewares, topicMiddlewaresOverride)
