c55716e8bd2a69426407a471f7d91e5eeb7df7ff
Luciano <lmilia0@rosario.gov.ar>
2019-05-03 07:33:47 -0300

    .

--- ext/lib/admin/boot/overrides.js
+++ ext/lib/admin/boot/overrides.js
@@ -12,3 +12,8 @@ import * as bodySerializer from 'lib/admin/admin-topics-form/body-serializer'
 import * as bodySerializerOverride from 'ext/lib/admin/admin-topics-form/body-serializer'
 
 bodySerializer.default = bodySerializerOverride.default
+
+import * as admin from 'lib/admin/admin/admin'
+import * as adminOverride from 'ext/lib/admin/admin/admin'
+
+admin.default = adminOverride.default
