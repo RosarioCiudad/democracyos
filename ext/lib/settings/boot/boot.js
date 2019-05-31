17402549d527623b5a563e6e7c71cc74d485a4e6
Luciano <lmilia0@rosario.gov.ar>
2019-05-31 10:19:50 -0300

    .

--- ext/lib/settings/boot/boot.js
+++ ext/lib/settings/boot/boot.js
@@ -1,3 +1,49 @@
 import 'ext/lib/boot/overrides'
-import 'lib/settings/boot/boot'
+
+
+/*import 'lib/settings/boot/boot'*/
+
+import 'native-promise-only'
+import page from 'page'
+import timeago from 'democracyos-timeago'
+import config from 'lib/config/config'
+
+import 'lib/boot/moment'
+import 'lib/translations/translations'
+
+/**
+ * Register routes aliases
+ */
+
+import 'lib/boot/routes'
+
+
+/**
+ * Mount applications.
+ */
+
+import 'lib/analytics'
+import 'lib/header/header'
+import 'lib/error-pages/error-pages'
+import '../settings/settings'
+import 'lib/settings/forum-new/forum-new'
+import 'lib/404/404'
 import './overrides'
+
+/**
+ * Init `timeago` component with parameter locale
+ */
+
+timeago('.ago', { lang: config.locale, interval: 1000 })
+
+/**
+ * Init page.js
+ */
+
+page()
+
+if (config.googleAnalyticsTrackingId) {
+  require('democracyos-ga')(config.googleAnalyticsTrackingId)
+}
+
+
