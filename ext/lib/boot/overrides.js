5351634b569cc134518851f1b1707b759a411b59
Luciano <lmilia0@rosario.gov.ar>
2019-05-31 10:21:24 -0300

    .

--- ext/lib/boot/overrides.js
+++ ext/lib/boot/overrides.js
@@ -9,3 +9,7 @@ import * as topicMiddlewaresOverride from 'ext/lib/middlewares/topic-middlewares
 
 Object.assign(topicMiddlewares, topicMiddlewaresOverride)
 
+import * as addUserInput from 'lib/admin/admin-permissions/add-user-input/add-user-input'
+import * as addUserInputOverride from 'ext/lib/admin/admin-permissions/add-user-input/add-user-input'
+
+addUserInput.default = addUserInputOverride.default
