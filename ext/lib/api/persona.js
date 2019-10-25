dbafcaba26060f83b8a1af068d7e783bc058e759
Luciano <lmilia0@rosario.gov.ar>
2019-10-25 12:20:27 -0300

    api persona con variable

--- ext/lib/api/persona.js
+++ ext/lib/api/persona.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const url = require('url')
-var config = require('lib/config')
+/*var config = require('lib/config')*/
 var l10n = require('lib/l10n')
 const request = require('superagent')
 const utils = require('lib/utils')
 const debug = require('debug')
+const config = require('ext/lib/config')
 
 /*const User = require('lib/models').User*/
 
@@ -14,7 +15,8 @@ const app = module.exports = express()
 
 app.get('/',
 function buscarPersona (req, res, next) {
-  const url = 'https://ws.rosario.gob.ar/persona/persona/fisica/' + req.query.dni + '/' + req.query.sexo
+  const url = config.ext.persona.url + req.query.dni + '/' + req.query.sexo
+  console.log(url)
    request
     .get(url)
     .query()
@@ -31,18 +33,3 @@ function buscarPersona (req, res, next) {
       }
     })
   })
- 
-
-
-
-/* const url = 'https://ws.rosario.gob.ar/persona/persona/fisica/' + req.query.dni + '/' + req.query.sexo
-    console.log(url)
-    return fetch(url)
-    .then(response => {
-      if (response.status == 200) {
-        let data = response.json()
-        return data
-      } else {
-          throw new Error(response.status)
-        }
-    })*/
